b02840d3ab2c1e0b6ae8f25fb9be948362d96ad3
Ceaser404 <119560473+Ceaser404@users.noreply.github.com>
2023-04-27 03:47:05 -0500

    added to path printing and asks for end city
    could probably output shortest path as a matrix with index and cost if that is more useful for SDL

--- main.cpp
+++ main.cpp
@@ -2,11 +2,32 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <stack>
 
 using namespace std;
 
 #define V 6		//No of vertices
 
+int findIndexCity(string nameCity)
+{
+    int indexOfCity;
+    if (nameCity == "Kansas City")
+        indexOfCity = 0;
+    else if (nameCity == "St. Lious")
+        indexOfCity = 1;
+    else if (nameCity == "Denver")
+		indexOfCity = 2;
+	else if (nameCity == "Minneapolis")
+        indexOfCity = 3;
+	else if (nameCity == "Eureka Springs")
+        indexOfCity = 4;
+	else if (nameCity == "Houston")
+		indexOfCity = 5;
+	else 
+		indexOfCity = 0;
+    
+    return indexOfCity;
+}
 
 string selectCityName(int number)
 {
@@ -52,8 +73,10 @@ int selectMinVertex(vector<int>& value,vector<bool>& processed)
 	return vertex;
 }
 
-void dijkstra(int graph[V][V])
+void dijkstra(int graph[V][V], string lastCity)
 {
+	int Fnsh = findIndexCity(lastCity);
+	cout << Fnsh << endl;
 	int parent[V];		//Stores Shortest Path Structure
 	vector<int> value(V,INT_MAX);	//Keeps shortest path values to each vertex from source
 	vector<string> nameCity(V, "KC");
@@ -63,7 +86,7 @@ void dijkstra(int graph[V][V])
 	parent[0] = -1;	//Start node has no parent
 	value[0] = 0;	//start node has value=0 to get picked 1st
 
-    for(int i = 0; i<V-1;i++){
+    for(int i = 0; i< V;i++){
 	    string name = selectCityName(i);
 	    nameCity[i] = name;        
     }
@@ -93,15 +116,46 @@ void dijkstra(int graph[V][V])
 		}
 	}
 	//Print Shortest Path Graph, currently shows the previous city visited to reach that city using the shortest path
-	for(int i=1;i<V;++i)
+	for(int i=1;i<V;++i) {
+		cout<<"U->V: "<<nameCity[parent[i]]<<"->"<<nameCity[i]<<"  wt = "<<graph[parent[i]][i]<<"\n";		
+	}
+	stack<int> stack;
+
+	cout << "\n" << " " << endl;
+	int i = Fnsh;
+	
+	while (i > 0) {
 		cout<<"U->V: "<<nameCity[parent[i]]<<"->"<<nameCity[i]<<"  wt = "<<graph[parent[i]][i]<<"\n";
+		stack.push(i);
+		i = parent[i];	
+			
+	}
+	cout << "\n";
+	stack.push(i);
+	int k = 0;
+	while (!stack.empty()) {
+		if (k != 0)
+			cout << " -> ";
+		cout << nameCity[stack.top()];
+		stack.pop();
+		k++;
+	}
+	cout <<"\n";
+
+
 
 	cout << "\n" << " " << endl;
 
-	for(int i=1;i<V;++i)
-		for(int j = 1;j <V;++j)
-		cout<<"U->V: "<<nameCity[i]<<"->"<<"  wt = "<<graph[i][j]<<"\n";
-
+	// Prints distance from each city to adjacent cities
+	for(int i=0;i<V;++i) {
+		for(int j = 1;j <V;++j) {
+			if (i == j && j + 1 < V)
+				j = j + 1;
+			if (graph[i][j] > 0) {
+			cout<<"U->V: "<<nameCity[i]<<"->"<< nameCity[j]<<"  wt = "<<graph[i][j]<<"\n";				
+			}	
+		}
+	}
 
 }
 //Users should be able to pick a city to go to and the entire shortest path should be shown to that city
@@ -115,7 +169,13 @@ int main()
 						{0, 7, 5, 4, 0, 7},
 						{0, 0, 0, 6, 7, 0} };
 
-	dijkstra(graph);
+
+    // City must be spelled correctly, still not working for cities with a space in name
+	string finish;
+	cout << "What city are you going to? " << "\n";
+	cin >>finish;
+
+	dijkstra(graph, finish);
 	
 	return 0;
 }
